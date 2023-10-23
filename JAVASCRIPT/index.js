817e5d3a73f8e57429ae03d17e7acb2314c7b4dc
egos9ntries <muruaaron@icloud.com>
2023-10-23 15:48:35 -0300

    se agrego mas colores a los semaforos

--- JAVASCRIPT/index.js
+++ JAVASCRIPT/index.js
@@ -19,14 +19,19 @@ let cambio = true;
 botonEncendido.addEventListener("click", () => {
     encendido = !encendido;
     if (encendido) {
-            luzRoja.style.background =  "#ff0000";
-            luzVerde1.style.background = "#00ff04";            
+        luzRoja.style.backgroundImage = "radial-gradient(circle, #ff0000, #800000)";
+        luzRoja.style.boxShadow = "0 0 10px rgba(255, 0, 0, 0.7)";
+
+        luzVerde1.style.backgroundImage = "radial-gradient(circle, #00ff04, #008000)";
+        luzVerde1.style.boxShadow = "0 0 10px rgba(0, 255, 0, 0.7)";
     }
     else {
             luzRoja.style.removeProperty("Background");
+            luzRoja.style.boxShadow = "0px 0px 5px 5px #000000";
             luzVerde.style.removeProperty("Background");
             luzRoja1.style.removeProperty("Background");
             luzVerde1.style.removeProperty("Background");
+            luzVerde1.style.boxShadow = "0px 0px 5px 5px #000000";
     }
 })
 
@@ -36,26 +41,58 @@ botonCambio.addEventListener("click", () => {
     }
    if (cambio) {
             luzRoja.style.removeProperty("Background");
+            luzRoja.style.boxShadow = "0px 0px 5px 5px #000000";
+
             luzVerde1.style.removeProperty("Background");
-            luzAmarilla.style.background = "#fffb00";
-            luzAmarilla1.style.background = "#fffb00";
+            luzVerde1.style.boxShadow = "0px 0px 5px 5px #000000";
+
+            luzAmarilla.style.backgroundImage = "radial-gradient(circle, #ffff00, #cccc00)";
+            luzAmarilla.style.boxShadow = "0 0 10px rgba(255, 255, 0, 0.7)";
+
+            luzAmarilla1.style.backgroundImage = "radial-gradient(circle, #ffff00, #cccc00)";
+            luzAmarilla1.style.boxShadow = "0 0 10px rgba(255, 255, 0, 0.7)";
+
             setTimeout(function() {
-                luzVerde.style.background = "#00ff04";
-                luzRoja1.style.background =  "#ff0000";
+                luzVerde.style.backgroundImage = "radial-gradient(circle, #00ff04, #008000)";
+                luzVerde.style.boxShadow = "0 0 10px rgba(0, 255, 0, 0.7)";
+
+                luzRoja1.style.backgroundImage = "radial-gradient(circle, #ff0000, #800000)";
+                luzRoja1.style.boxShadow = "0 0 10px rgba(255, 0, 0, 0.7)";
+
                 luzAmarilla.style.removeProperty("Background");
+                luzAmarilla.style.boxShadow = "0px 0px 5px 5px #000000";
+
                 luzAmarilla1.style.removeProperty("Background");
+                luzAmarilla1.style.boxShadow = "0px 0px 5px 5px #000000";
+
             }, 3000);
         }
         else {
             luzRoja1.style.removeProperty("Background");
+            luzRoja1.style.boxShadow = "0px 0px 5px 5px #000000";
+
             luzVerde.style.removeProperty("Background");
-            luzAmarilla.style.background = "#fffb00";
-            luzAmarilla1.style.background = "#fffb00";
+            luzVerde.style.boxShadow = "0px 0px 5px 5px #000000";
+
+            luzAmarilla.style.backgroundImage = "radial-gradient(circle, #ffff00, #cccc00)";
+            luzAmarilla.style.boxShadow = "0 0 10px rgba(255, 255, 0, 0.7)";
+
+            luzAmarilla1.style.backgroundImage = "radial-gradient(circle, #ffff00, #cccc00)";
+            luzAmarilla1.style.boxShadow = "0 0 10px rgba(255, 255, 0, 0.7)";
+
             setTimeout(function() {
-                luzVerde1.style.background = "#00ff04";
-                luzRoja.style.background =  "#ff0000";
+                luzVerde1.style.backgroundImage = "radial-gradient(circle, #00ff04, #008000)";
+                luzVerde1.style.boxShadow = "0 0 10px rgba(0, 255, 0, 0.7)";
+
+                luzRoja.style.backgroundImage = "radial-gradient(circle, #ff0000, #800000)";
+                luzRoja.style.boxShadow = "0 0 10px rgba(255, 0, 0, 0.7)";
+
                 luzAmarilla.style.removeProperty("Background");
+                luzAmarilla.style.boxShadow = "0px 0px 5px 5px #000000";
+
                 luzAmarilla1.style.removeProperty("Background");
+                luzAmarilla1.style.boxShadow = "0px 0px 5px 5px #000000";
+
             }, 3000);
         }
     cambio = !cambio;
